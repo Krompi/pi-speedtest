11bdac76502fffc34574d6986a680a8821f2874b
Krompi <krompi@gmail.com>
2018-05-07 21:25:31 +0200

    Erster Versuch, den Zeitraum einzuschränken

--- graph.php
+++ graph.php
@@ -14,16 +14,27 @@ $parameter    = explode("-", substr(basename($_SERVER["REQUEST_URI"]), 0, strpos
 $parPrecision = $parameter[1];
 $parPeriod    = $parameter[2];
 
+$sqlGroup = "";
+$sqlWhere = "";
+$sqlOrder = "";
+
 if ( $parPrecision == "tag" ) {
     $sql  = "SELECT YEAR(time) AS year, MONTH(time) AS month, DAY(time) AS day, AVG(down) AS down_avg, MIN(down) AS down_min, MAX(down) AS down_max 
-               FROM results 
-           GROUP BY  YEAR(time), MONTH(time), DAY(time)";
+               FROM results";
+    $sqlGroup = " GROUP BY  YEAR(time), MONTH(time), DAY(time)";
+    $sqlOrder = " ORDER BY year, month, day";
 } elseif ( $parPrecision == "stunde" ) {
     $sql  = "SELECT YEAR(time) AS year, MONTH(time) AS month, DAY(time) AS day, HOUR(time) AS hour, AVG(down) AS down_avg, MIN(down) AS down_min, MAX(down) AS down_max 
-               FROM results 
-           GROUP BY  YEAR(time), MONTH(time), DAY(time), HOUR(time)
-           ORDER BY year, month, day, hour";
+               FROM results ";
+    $sqlGroup = " GROUP BY  YEAR(time), MONTH(time), DAY(time), HOUR(time)";
+    $sqlOrder = " ORDER BY year, month, day, hour";
 }
+
+if ( $parPeriod == "heute" ) {
+    $sqlWhere .= " WHERE YEAR(time)='".date("Y")."' AND MONTH(time)='".date("m")."' AND DAY(time)='".date("d")."'";
+}
+$sql = $sql.$sqlWhere.$sqlGroup.$sqlOrder;
+echo $sql;
 
 
 foreach ( $dbh->query( $sql ) as $row ) {
